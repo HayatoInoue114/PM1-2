d9dbc4f729d4c68b276e73f1b617e07962f6c706
k022g0087@g.neec.ac.jp <k022g0087@g.neec.ac.jp>
2022-10-12 11:13:10 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+	printf("Hello GitHub World\n");
+
+	printf("Hello New branch\n");
+
+	int a = 0;
+	while (a != 1) {
+		printf("\nコンソールアプリケーションを終了する場合は数字の1を入力しEnterを押す");
+		scanf_s("%d", &a);
+	}
+
+	return 0;
+}
